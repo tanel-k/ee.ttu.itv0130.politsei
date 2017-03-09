aeafa31cc265b094255233b4ba9e621dbe522903
Tanel Prikk <tanel.prikk@outlook.com>
2017-03-09 23:42:48 +0200

    bootstrap confirmation

---
+++ src/resources/elements/confirm-button.js
@@ -0,0 +1,28 @@
+import {bindable, bindingMode, inject} from 'aurelia-framework';
+import 'jquery';
+
+@inject(Element)
+export class ConfirmButton {
+	@bindable({ defaultBindingMode: bindingMode.oneWay }) icon = 'cross';
+	@bindable({ defaultBindingMode: bindingMode.oneWay }) classes = '';
+	@bindable({ defaultBindingMode: bindingMode.oneWay }) placement = 'top';
+	@bindable({ defaultBindingMode: bindingMode.oneWay }) ok = 'Yes';
+	@bindable({ defaultBindingMode: bindingMode.oneWay }) cancel = 'No';
+	@bindable({ defaultBindingMode: bindingMode.oneWay }) content = '';
+	@bindable({ defaultBindingMode: bindingMode.oneWay }) title = 'Are you sure?';
+	
+	@bindable action;
+	
+	constructor(element) {
+		this.element = element;
+	}
+
+	attached() {
+		this.selector = $(this.element).find('button')
+			.confirmation();
+	}
+	
+	detached() {
+		
+	}
+}
